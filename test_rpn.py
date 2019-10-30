9e881614c1f92140ec7097a209b71e99e5833f14
Tiny Dantzer <dantzert@umich.edu>
2019-10-30 14:19:15 -0400

    add tests for division
    cover both types, even division (no remainder) and floor division

--- test_rpn.py
+++ test_rpn.py
@@ -19,3 +19,11 @@ class TestBasics(unittest.TestCase):
 	def test_mul(self):
 		result = rpn.calculate('4 8 *')
 		self.assertEqual(32,result)
+
+	def test_div1(self):
+		result = rpn.calculate("5 3 /")
+		self.assertEqual(1,result)
+
+	def test_div2(self):
+		result = rpn.calculate('12 3 /')
+		self.assertEqual(4,result)
